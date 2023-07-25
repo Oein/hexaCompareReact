b10637af2ec8a6177f5dd9b4a9f3867a158b997d
Oein <oein0219@gmail.com>
2023-07-25 14:20:59 +0900

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./logo.css";
+
+function Logo() {
+  return (
+    <a target="_blank" href="__props_link__">
+      <img src={require("./hexaLogo.png")} className="logo" />
+    </a>
+  );
+}
+
+function FlowItem(props: { name: string; color: string }) {
+  return (
+    <div
+      style={{
+        color: props.color,
+        padding: "15px",
+      }}
+    >
+      <span className="hexaPlus">Hexa +&nbsp;</span>
+      {props.name}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <div>
+        <span className="title">
+          <span className="flowContainer">
+            <span className="flower">
+              <FlowItem name="HTML" color="#f1652a" />
+              <FlowItem name="Typescript" color="#2f74c0" />
+              <FlowItem name="Vite" color="#9666fd" />
+              <FlowItem name="HTML" color="#f1652a" />
+            </span>
+          </span>
+        </span>
+      </div>
+      <p className="read-the-docs">Click on the Hexa logo to learn more</p>
     </div>
   );
 }
